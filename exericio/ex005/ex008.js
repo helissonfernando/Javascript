634852d70a46af2c1c4d6e0a096e0d304b4a2196
Helisson Fernando <121846338+helissonfernando@users.noreply.github.com>
2023-01-25 21:20:30 -0300

    estudo de js

---
+++ exericio/ex005/ex008.js
@@ -0,0 +1,8 @@
+var vel = 45.1
+
+console.log(`A velocidade do seu caro é ${vel}Km/h`)
+
+if (vel > 60){ // condição simples
+    console.log(`voce ultrapassou a velocidade permitada. Multado!`)
+}
+console.log(`Dirigja sempre usando cinto de segurança !`)
